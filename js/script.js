ef8bf24f25abcbce8ce211b7a6015ba783aa247f
ajayidhikrullah <ajayidhikrullah@gmail.com>
2018-10-21 12:51:33 +0100

    Our first commit with Nurudeen my Lovely Brother

---
+++ js/script.js
@@ -0,0 +1,46 @@
+//Javascript basics
+/* This is how to comment many things is JS*/
+
+/*var myVar = 'Welcome to Js bsics'
+document.write(myVar);
+alert(myVar);
+var a = 20;
+if(a == 10) {
+    alert('a value is Wrong');
+} else {
+    alert('this is true ooo');
+}
+*/
+
+// function myFun() {
+//     alert('<br> code execution of funtion by SK');
+// }
+
+// myFun();
+
+/*_________Defining an Object In JS__________
+var person = {
+    first_name: 'Sikiru',
+    last_name: 'Ajayi',
+    tall: 6.6,
+    age: 24,
+}
+
+document.write('<br>' + person.first_name + "<br>");
+document.write(person.last_name + "<br");
+
+let school = 'Al-Izzah';
+document.getElementById('sch').innerHTML = school;*/
+
+// var urIdentity = {
+//     firstName: 'Ajayi',
+//     lastName: 'Adekunle',
+//     graduateYear: 2016,
+//     serviceYear: 201819,
+//     fullDetails: function(me) {
+//         return this.firstName + ' ' + this.lastName + '. I graduated in the year ' + this.serviceYear;
+//     }
+// };
+
+// document.getElementById('sch').innerHTML = urIdentity.fullDetails();
+
